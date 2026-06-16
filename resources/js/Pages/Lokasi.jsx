cedacd19ec6954f2f415a8188ae1ca9277129e4b
Delvitri <deltadavidrii@gmail.com>
2026-06-16 18:21:15 +0700

    sudah sampai acuan lokasi

--- resources/js/Pages/Lokasi.jsx
+++ resources/js/Pages/Lokasi.jsx
@@ -18,13 +18,25 @@ export default function Lokasi() {
       
 <div class="w-full px-6">
     
-    <div class="max-w-7xl mx-auto bg-gray-200 rounded-lg p-8 shadow-sm text-center">
-       <div className="w-10 h-10 bg-red-500 rounded-full"></div>
+    <div class="max-w-7xl mx-auto bg-gray-200 rounded-lg p-8 shadow-sm ">
+
+        <div className="flex items-center gap-4 mb-4">
+       <div className="w-10 h-10 bg-red-500 rounded-full flax-shrink-0"></div>
+       <p class= "text-gray-500 font-medium text-lg leading-relaxed">Sangat membutuhkan donor pengganti.</p>
+           </div>
        
-       <div className="w-10 h-10 bg-red-500 rounded-full"></div>
-       <div className="w-10 h-10 bg-red-500 rounded-full"></div>
+       <div className="flex items-center gap-4 mb-4">
+       <div className="w-10 h-10 bg-yellow-400 rounded-full lax-shrink-0"></div>
+        <p class= "text-gray-500 font-medium text-lg leading-relaxed">Persediaan darah masih ada untuk beberapa hari ke depan.</p>
+     </div>
+
+       <div className="flex items-center gap-4 mb-4">
+       <div className="w-10 h-10 bg-emerald-500 rounded-full lax-shrink-0"></div>
+        <p class= "text-gray-500 font-medium text-lg leading-relaxed">Jumlah kantong darah yang tersedia & mampu meng-cover permintaan rutin.</p>
+           
+       </div>
         
-        <p class="text-red-500 text-sm">minimal 45 kg</p>
+        
     </div>
 </div>
 
